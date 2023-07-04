aec8415410bff7f726546d9ab027e1d3b478cb56
EugeneMakita <e.makita@qobogroup.com>
2023-07-04 11:34:57 +0200

    changes

--- src/ModuleConfig/ClassFactory.php
+++ src/ModuleConfig/ClassFactory.php
@@ -41,7 +41,7 @@ class ClassFactory
         $classes = Configure::read('ModuleConfig');
         $classMap = empty($options['classMap'][$classMapVersion]) ? Configure::read('ModuleConfig.classMap.' . $classMapVersion) : (array)$options['classMap'][$classMapVersion];
         if (empty($classMap[$configType][$classType])) {
-            throw new RuntimeException("Configuration: " . print_r($class, true));
+            throw new RuntimeException("Configuration: " . print_r($classes, true));
         }
     }
 
